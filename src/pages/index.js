6308e404b258fcb15d241068768d3b8b3660c22b
abzara20 <azaragozapalos@gmail.com>
2021-10-13 15:35:42 -0400

    complete nav component and first segment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "semantic-ui-css/semantic.css";
-import {} from "semantic-ui-react";
+import { Container, Segment, Header, Button } from "semantic-ui-react";
+import Nav from "../components/nav";
 //import {Link} from 'gatsby';
 
 const HomePage = () => {
   return (
     <React.Fragment>
-      <h1>Hello</h1>
+      <Nav></Nav>
+
+      <Container>
+        <Segment textAlign="center">
+          <Header>HOME</Header>
+          <p>Some text that needs not readen</p>
+          <Button color="green">Join Now!</Button>
+        </Segment>
+      </Container>
     </React.Fragment>
   );
 };
