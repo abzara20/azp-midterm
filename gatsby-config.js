0c51d8d2de74e59b5c24dfa8927612ee5b89b616
abzara20 <azaragozapalos@gmail.com>
2021-10-13 16:45:26 -0400

    build and update readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl:
+      "https://in-info-web4.informatics.iupui.edu/~abzara/n322/azp-midterm/",
     title: "azp-midterm",
   },
   plugins: [],
+  pathPrefix: "/~abzara/n322/azp-midterm/",
 };
